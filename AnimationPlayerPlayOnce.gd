76c3a992e34228e3f487737b027a69bc75227c90
Zshandi Krahn <ZshandiK@gmail.com>
2024-06-27 22:12:44 -0700

    Create AnimationPlayerPlayOnce helper for only playing animations once

---
+++ AnimationPlayerPlayOnce.gd
@@ -0,0 +1,16 @@
+extends AnimationPlayer
+class_name AnimationPlayerPlayOnce
+
+var last_played:StringName = ""
+
+func _ready():
+	animation_started.connect(
+		func(anim_name: StringName):
+			last_played = anim_name
+	)
+
+func play_once(name: StringName = "", custom_blend: float = -1, custom_speed: float = 1.0, from_end: bool = false):
+	if last_played != name:
+		play(name, custom_blend, custom_speed, from_end)
+		last_played = name
+
