282ed3c945265ff9d0ea4372ed8899b1ec531e57
Zshandi Krahn <ZshandiK@gmail.com>
2024-06-30 23:05:18 -0700

    Add shape animation and delay
    Add shape animation and delay before returning control
    
    Fade points away, then make shape thicker and colored
    
    Also made dots smaller when clicked, and lines thicker

--- PointConnectionController.gd
+++ PointConnectionController.gd
@@ -19,7 +19,7 @@ func _ready():
 func clear():
 	connected_points.clear()
 	for point in points:
-		point.is_connected = false
+		point.reset()
 	
 	connected_lines.clear()
 	current_line = null
@@ -39,19 +39,47 @@ func complete_shape(point:ConnectablePoint):
 		connected_points.pop_front()
 		connected_lines.pop_front()
 	
-	#TODO: Add timer and/or animation etc.
+	var tween = create_tween().set_parallel(true)
+	
+	# Hide all points
+	for point_i in points:
+		tween.tween_property(point_i, "modulate", Color.TRANSPARENT, 1)
+	# Once hidden, each one should be reset
+	tween.set_parallel(false)
+	tween.tween_callback(
+		func ():
+			print_debug("points hidden")
+			for point_i in points:
+				point_i.reset()
+	)
+	tween.set_parallel(true)
+	
+	# Animate the lines
+	for line in connected_lines:
+		tween.tween_property(line, "width", line.width + 10, 3).set_trans(Tween.TRANS_SPRING)
+		tween.tween_property(line, "color", Color.RED, 2).set_trans(Tween.TRANS_CUBIC)
+	
+	# Wait for all animations to complete, then delay and call back to done
+	tween.set_parallel(false)
+	tween.tween_interval(2)
+	tween.tween_callback(
+		func ():
+			print_debug("Tween done!")
+			for point_i in points:
+				point_i.modulate = Color.WHITE
+			clear()
+			completing_shape = false
+	)
 
 func _on_touch_input_primary_touch_event(event, longest_touched_point):
+	
+	if completing_shape:
+		# We don't allow any interaction while completing the shape
+		return
 	
 	if longest_touched_point == null:
 		# Always clear when touch released
 		clear()
-		#TODO: Temp for debug
-		completing_shape = false
-		return
-	
-	if completing_shape:
-		# We don't allow any interaction while completing the shape
 		return
 	
 	# Check if each point is affected by touch
@@ -87,7 +115,7 @@ func connect_point(point:ConnectablePoint):
 	connected_lines.push_back(current_line)
 
 func add_line(from:Vector2) -> CappedLine:
-	var line = CappedLine.create(from, Vector2.ZERO, 20, Color.WHITE, true)
+	var line = CappedLine.create(from, Vector2.ZERO, 30, Color.WHITE, true)
 	line.owner = owner
 	line_container.add_child(line)
 	return line
