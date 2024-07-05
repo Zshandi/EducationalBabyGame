77004fa039c6a66769146e54c6a265a83da184e8
Zshandi Krahn <ZshandiK@gmail.com>
2024-07-05 00:12:30 -0700

    Add piano note, which is then pitched to create ascending notes when lines connected
    Still need to make shape completed sound, and fix the ratio function in main

--- PointConnectionController.gd
+++ PointConnectionController.gd
@@ -28,6 +28,7 @@ func clear():
 
 func complete_shape(point:ConnectablePoint):
 	completing_shape = true
+	Main.instance.play_note(connected_points.size()+1)
 	
 	# Connect the current line to the point
 	current_line.to = point.global_position
@@ -121,6 +122,7 @@ func connect_point(point:ConnectablePoint):
 		current_line.to = point.position
 	current_line = add_line(point.position)
 	connected_lines.push_back(current_line)
+	Main.instance.play_note(connected_points.size())
 
 func add_line(from:Vector2) -> CappedLine:
 	var line = CappedLine.create(from, Vector2.ZERO, 30, Color.WHITE, true)
--- main.gd
+++ main.gd
@@ -1,7 +1,47 @@
 extends Node2D
 class_name Main
 
+static var twelfth_root := 1.05946309436
+
 static var instance:Main
 
 func _init():
 	instance = self
+
+func play_note(step:int):
+	var pitch_semitone = step - 12
+	var pitch_scale = get_pitch_scale_for(pitch_semitone)
+	adjust_pitch_scale(pitch_scale, 0.4, $AudioStreamPlayer)
+	$AudioStreamPlayer.play()
+
+func adjust_pitch_scale(pitch_scale:float, stream_to_effect_ratio:float,
+	stream_player:AudioStreamPlayer, effect:AudioEffectPitchShift = null):
+	
+	var bus_idx := AudioServer.get_bus_index(stream_player.bus)
+	# Null effect means use first pitch shift effect on stream player's bus,
+	#  or create one if we can't find it
+	if effect == null:
+		for effect_idx in AudioServer.get_bus_effect_count(bus_idx):
+			var effect_inst = AudioServer.get_bus_effect(bus_idx, effect_idx)
+			if effect_inst is AudioEffectPitchShift:
+				effect = effect_inst
+				break
+		if effect == null:
+			effect = AudioEffectPitchShift.new()
+			AudioServer.add_bus_effect(bus_idx, effect)
+	
+	# Do some math to find each pitch scale
+	var stream_pitch = sqrt(pitch_scale / stream_to_effect_ratio)
+	var effect_pitch = pitch_scale / stream_pitch
+	
+	# Adjust the scale
+	stream_player.pitch_scale = stream_pitch
+	effect.pitch_scale = effect_pitch
+
+func get_pitch_scale_for(semitones:int):
+	# Each octave doubles (or halves) the pitch
+	# There are 12 semitones in an octave,
+	#  so each semitone multiplies by 12th root of 2
+	var pitch_scale:float = pow(twelfth_root, semitones)
+	
+	return pitch_scale
